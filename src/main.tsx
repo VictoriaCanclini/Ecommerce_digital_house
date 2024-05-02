04c6c8234d9a6204c393e20134ebf0951b8df5f9
Victoria Canclini <vico_canclini@hotmail.com>
2024-05-02 11:46:39 -0300

    conf inicial

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { LayoutMain } from "./components/Layouts/LayoutMain.tsx";
+import Home from "./pages/Home/Home.tsx";
+import Cart from "./pages/Cart/Cart.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LayoutMain />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/cart", element: <Cart /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
