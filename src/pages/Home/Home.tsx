dbe0af0b55b0959855eaf35cf1e8c569d7904de1
Victoria Canclini <vico_canclini@hotmail.com>
2024-05-08 12:10:40 -0300

    ok pagination

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,19 @@
-import { useEffect, useState } from "react";
 import { Hero } from "../../components/ui/Hero";
 import styles from "./Home.module.css";
 import { CardProduct } from "../../components/ui/CardProduct";
 import { getProducts } from "../../service";
-import { Product } from "../../interface";
 import { Toaster } from "sonner";
+import { useQuery } from "react-query";
+import { useState } from "react";
 
 const home = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    getProducts()
-      .then((data) => {
-        setProducts(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
+  const { data, isLoading, error } = useQuery(
+    ["products", page],
+    () => getProducts(page),
+    { keepPreviousData: true }
+  );
 
   return (
     <>
@@ -31,9 +22,27 @@ const home = () => {
       {isLoading && <p>Loading...</p>}
       {error && <p>Something went wrong</p>}
       <div className={styles.container}>
-        {products.map((product) => (
+        {data?.map((product) => (
           <CardProduct key={product.tail} product={product} />
         ))}
+      </div>
+      <div className={styles.paginationContainer}>
+        <button
+          onClick={() => setPage(page - 1)}
+          className={styles.paginationButton}
+          disabled={page === 1}
+        >
+          Previus page
+        </button>
+        <div className={styles.paginationActive}>
+          <span>{page}</span>
+        </div>
+        <button
+          className={styles.paginationButton}
+          onClick={() => setPage(page + 1)}
+        >
+          Next page
+        </button>
       </div>
     </>
   );
--- src/service/products.service.ts
+++ src/service/products.service.ts
@@ -1,8 +1,12 @@
 import { Product } from "../interface";
 
-export const getProducts = async (): Promise<Product[]> => {
+export const getProducts = async (page = 0): Promise<Product[]> => {
   try {
-    const response = await fetch("http://localhost:3000/products");
+    const response = await fetch(
+      `http://localhost:3000/products?_start=${page}&_limit=14`
+    );
+
+    console.log(response);
 
     if (response.ok) {
       const data = await response.json();
