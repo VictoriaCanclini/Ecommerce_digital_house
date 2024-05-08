4b213e96c5f162e872c1542c125ac07ba12b93b3
Victoria Canclini <vico_canclini@hotmail.com>
2024-05-08 19:10:32 -0300

    finish

--- src/service/products.service.ts
+++ src/service/products.service.ts
@@ -6,13 +6,32 @@ export const getProducts = async (page = 0): Promise<Product[]> => {
       `http://localhost:3000/products?_start=${page}&_limit=14`
     );
 
-    console.log(response);
-
     if (response.ok) {
       const data = await response.json();
       return data;
     } else {
       throw new Error("Faild fetch products");
+    }
+  } catch (error) {
+    throw new Error("Network error");
+  }
+};
+
+export const createProduct = async (product: Product): Promise<Product> => {
+  try {
+    const response = await fetch("http://localhost:3000/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error("Faild to create product");
     }
   } catch (error) {
     throw new Error("Network error");
